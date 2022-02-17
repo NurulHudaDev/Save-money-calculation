49a0b78a9deda904bab913f4717f30d8bdfb0648
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-02-17 13:42:08 +0300

    assignment-5

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-document.getElementById('calculate-btn').addEventListener('click', function(){
+function calculateBtn(){
     // Income
-    const incomeInputValue = document.getElementById('income').value;
-    const NewInputBtnIncome = parseInt(incomeInputValue);
-
+    const incomeInputValue = document.getElementById('income');
     // Expenses
-    const foodInputValue = document.getElementById('food').value;
-    const NewInputBtnFood = parseInt(foodInputValue);
-    
-    const rentInputValue = document.getElementById('rent').value;
-    const NewInputBtnRent = parseInt(rentInputValue);
+    const foodInputValue = document.getElementById('food');
+    const rentInputValue = document.getElementById('rent');
+    const clothesInputValue = document.getElementById('clothes');
 
-    const clothesInputValue = document.getElementById('clothes').value;
-    const NewInputBtnClothes = parseInt(clothesInputValue);
-    
-    // Expenses total
-    const calculateTotal = NewInputBtnFood + NewInputBtnRent + NewInputBtnClothes;
-    document.getElementById('total-expenses').innerText = calculateTotal;
-    
-    // Total balance
-    const totalBalance = NewInputBtnIncome - calculateTotal;
-    document.getElementById('balance').innerText = totalBalance;
+    const ExpensesOutput = document.getElementById('total-expenses');
+    ExpensesOutput.innerText = parseFloat(foodInputValue.value) + parseFloat(rentInputValue.value) + parseFloat(clothesInputValue.value);
+    const balance = document.getElementById('balance');
+    balance.innerText = parseFloat(incomeInputValue.value) - ExpensesOutput.innerText;
+}
 
+// Save
+function saveBtn(){
+   const incomeValue = document.getElementById('income');
+   const saveInput = document.getElementById('save-input');
 
+    const savingAmount = document.getElementById('Saving-amount');
+    savingAmount.innerText = parseFloat(incomeValue.value) * parseFloat(saveInput.value) / 100;
 
-
-})
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = balance.innerText - savingAmount.innerText;
+}
 
 
