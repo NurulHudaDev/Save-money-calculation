fb63ce6d7b552b6a91d9f26acb9e6029e3c000d9
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-02-17 20:15:47 +0300

    assignment-5

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 function calculateBtn(){
-    // Income
+// Income input
     const incomeInputValue = document.getElementById('income');
-    // Expenses
+// Expenses input
     const foodInputValue = document.getElementById('food');
     const rentInputValue = document.getElementById('rent');
     const clothesInputValue = document.getElementById('clothes');
-
+// Total expenses
     const ExpensesOutput = document.getElementById('total-expenses');
     ExpensesOutput.innerText = parseFloat(foodInputValue.value) + parseFloat(rentInputValue.value) + parseFloat(clothesInputValue.value);
     const balance = document.getElementById('balance');
+// Balance
     balance.innerText = parseFloat(incomeInputValue.value) - ExpensesOutput.innerText;
-
+// 3 Error message
     if(incomeInputValue.value == '' && foodInputValue.value == '' && rentInputValue.value == '' && clothesInputValue.value ==''){
         alert('please input field type');
     }
@@ -26,10 +27,10 @@ function calculateBtn(){
 function saveBtn(){
    const incomeValue = document.getElementById('income');
    const saveInput = document.getElementById('save-input');
-
+// Saving amount
     const savingAmount = document.getElementById('Saving-amount');
     savingAmount.innerText = parseFloat(incomeValue.value) * parseFloat(saveInput.value) / 100;
-
+// Remaining balance
     const remainingBalance = document.getElementById('remaining-balance');
     remainingBalance.innerText = balance.innerText - savingAmount.innerText;
 }
